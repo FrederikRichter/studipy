19e244bf3670b13488eacae06cf406ca811f3818
FrederikRichter <Frederik.Richter@mailbox.org>
2024-01-23 14:28:21 +0100

    added docstrings

--- studipy/browser.py
+++ studipy/browser.py
@@ -2,19 +2,23 @@ import requests
 import json
 
 class StudipError(Exception):
+    """default Exception if Studip Request fails"""
     pass
 
 def get(url, auth, params={}) -> object:
+    """returns json response from api"""
     response = requests.get(url, auth=auth, params=params)
     check_status(status_code=response.status_code, url=url)
     return json.loads(response.text)
 
 def download(url, auth) -> bytes:
+    """returns binary response from api"""
     response = requests.get(url, auth=auth, stream=True)
     check_status(status_code=response.status_code, url=url)
     return response.content
 
 def check_status(status_code, url):
+    """raises error based on status code"""
     match status_code:
         case 401:
             raise StudipError(f"Unauthorized! tried accessing {url} with status code {status_code}")
@@ -22,3 +26,4 @@ def check_status(status_code, url):
             raise StudipError(f"Forbidden! tried accessing {url} with status code {status_code}")
         case 404:
             raise StudipError(f"Not Found! tried accessing {url} with status code {status_code}")
+
--- studipy/client.py
+++ studipy/client.py
@@ -3,61 +3,79 @@ import studipy.browser as browser
 
 class Client:
     def __init__(self, username, password, base_url):
+        """Init studipy Client.
+        Keyword arguments:
+        username -- the studIP username (like normal login)
+        password -- the studIP password (...)
+        base url -- studIP instancce base url (https://studip.example.com/)
+        """
         self._auth = (username, password)
         self._api_url = f'{base_url}jsonapi.php/v1/'
         self.me = browser.get(self._api_url + "users/me", auth=self._auth)
 
     def get_courses(self) -> object:
+        """returns json list of courses the user is in"""
         return browser.get(
             self._api_url + "users/" + self.me + "/courses/",
-            params={"page[limit]" : "-1"},
+            params={"page[limit]": "-1"},
             auth=self._auth
-            )
-    
+        )
+
     def view_course(self, course_id) -> object:
+        """returns json data about a specific course, need course id"""
         return browser.get(
             self._api_url + "courses/" + course_id,
             auth=self._auth
-            )
-    
+        )
+
     def get_course_folders(self, course_id) -> object:
+        """returns json list of folders inside a specific course, needs course id"""
         return browser.get(
             self._api_url + "courses/" + course_id + "/folders",
             auth=self._auth
-            )
-    
+        )
+
     def get_course_files(self, course_id) -> object:
+        """returns json list of file refs in a specific course, needs course id"""
         return browser.get(
             self._api_url + "courses/" + course_id + "/file-refs",
             auth=self._auth
-            )
-    
+        )
+
     def get_subfolders(self, folder_id) -> object:
+        """returns json list of folders inside a specific folder, needs folder id"""
         return browser.get(
             self._api_url + "folders/" + folder_id + "/folders",
             auth=self._auth
-            )
+        )
 
     def get_folder_files(self, folder_id) -> object:
         return browser.get(
             self._api_url + "folders/" + folder_id + "/file-refs",
             auth=self._auth
-            )
+        )
 
     def get_users(self, limit=30) -> object:
+        """returns json list of userdata
+        Keyword arguments:
+        limit -- the max amount of users listed. limit of -1 lists all
+        """
         return browser.get(
-            self._api_url + "users", params={"page[limit]" : limit},
+            self._api_url + "users", params={"page[limit]": str(limit)},
             auth=self._auth
-            )
-    
-    def get_messages(self, filter_unread=0) -> object:
+        )
+
+    def get_messages(self, filter_unread=False) -> object:
+        """returns json list of user messages. can filter unread messages"""
         return browser.get(
             self._api_url + "users/" + self.me + "/inbox",
-            params={"filter[unread]" : str(filter_unread)},
+            params={"filter[unread]": str(int(filter_unread))},
             auth=self._auth
-            )
-    
+        )
+
     def download_file(self, file_id) -> bytes:
+        """returns bytes of file content, needs specific file id"""
         return browser.download(
             self._api_url + "file-refs/" + file_id + "/content", auth=self._auth
-            )
+        )
+
