5a97ddb6f39cea822b3dce571e12c9b553f3b0b6
Frederik Richter <frederik.richter@mailbox.org>
2024-06-29 03:02:07 +0200

    update examples

--- examples/studip-inbox.py
+++ examples/studip-inbox.py
@@ -9,7 +9,8 @@ password = os.getenv("STUDIP_LOGINSECRET")
 base_url = os.getenv("STUDIP_BASEURL")
 
 client = studipy.Client(username=username, password=password, base_url=base_url)
-messages = client.get_messages(filter_unread=True)
+messages = client.Messages.get_messages(filter_unread=True)
 
-for m in messages["data"]:
-    print(m["attributes"]["subject"])
+# print message subjects
+for m in messages:
+    print(m.Title)
